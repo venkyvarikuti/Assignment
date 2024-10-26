34d269f925664bb72b7fd4645df6731dd2226286
venkatesh <venkatesh@futy.uk>
2024-10-27 00:41:48 +0530

    handling all the other resources

--- src/app.js
+++ src/app.js
@@ -20,6 +20,9 @@ app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/tasks', taskRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).json({ message: 'Resource not found' });
+});
 // Error handling and other middleware here...
 
 const PORT = process.env.PORT || 8000;
